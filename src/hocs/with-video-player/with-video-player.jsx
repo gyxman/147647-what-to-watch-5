d49cfbbb9f0892e6d11af896b3bb5736fcc30baf
Roman Aleksyutin <gyxman@yandex.ru>
2020-11-07 23:14:59 +0300

    add hw

--- src/hocs/with-video-player/with-video-player.jsx
+++ src/hocs/with-video-player/with-video-player.jsx
@@ -8,11 +8,16 @@ const withVideoPlayer = (Component) => {
 
       this.state = {
         isPlaying: false,
+        isFullSize: false,
       };
 
       this.timerId = null;
       this.mouseEnter = this.mouseEnter.bind(this);
       this.mouseLeave = this.mouseLeave.bind(this);
+      this.openFullSize = this.openFullSize.bind(this);
+      this.closeFullSize = this.closeFullSize.bind(this);
+      this.playFullSize = this.playFullSize.bind(this);
+      this.pauseFullSize = this.pauseFullSize.bind(this);
     }
 
     mouseEnter() {
@@ -25,19 +30,40 @@ const withVideoPlayer = (Component) => {
       this.setState({isPlaying: false});
     }
 
+    openFullSize() {
+      this.setState({isFullSize: true, isPlaying: true});
+    }
+
+    closeFullSize() {
+      this.setState({isFullSize: false, isPlaying: false});
+    }
+
+    playFullSize() {
+      this.setState({isPlaying: true});
+    }
+
+    pauseFullSize() {
+      this.setState({isPlaying: false});
+    }
+
     render() {
-      const {isPlaying} = this.state;
+      const {isPlaying, isFullSize} = this.state;
 
       return <Component
         {...this.props}
+        openFullSize={this.openFullSize}
         renderPlayer={(src, poster) => {
           return (
             <VideoPlayer
               src={src}
               isPlaying={isPlaying}
+              isFullSize={isFullSize}
               poster={poster}
               onMouseEnter={this.mouseEnter}
               onMouseLeave={this.mouseLeave}
+              closeFullSize={this.closeFullSize}
+              playFullSize={this.playFullSize}
+              pauseFullSize={this.pauseFullSize}
             />
           );
         }}
